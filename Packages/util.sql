0c57e085b311e6d318607a8fcaa473aef0632b00
Dima <59514856+D1odessa@users.noreply.github.com>
2023-07-05 00:36:45 +0300

    Update util.sql
    добавил спецификацию и body

--- Packages/util.sql
+++ Packages/util.sql
@@ -1,3 +1,74 @@
+create or replace PACKAGE util AS
+
+    --переменные
+    gc_min_salary CONSTANT NUMBER := 2000;
+    
+    TYPE rec_value_list IS RECORD (value_list VARCHAR2(100));
+    TYPE tab_value_list IS TABLE OF rec_value_list;
+    
+    TYPE rec_exchange IS RECORD (   r030 NUMBER,
+                                    txt VARCHAR2(100),
+                                    rate NUMBER,
+                                    cur VARCHAR2(100),
+                                    exchangedate DATE );
+    TYPE tab_exchange IS TABLE OF rec_exchange;
+ 
+    TYPE rec_regions_list IS RECORD ( REGION_NAME VARCHAR2(100),
+                                      EMP_NUM NUMBER);
+    TYPE tab_regions_list IS TABLE OF rec_regions_list; 
+ 
+    --функции
+
+    FUNCTION add_year ( p_date in DATE DEFAULT sysdate,
+                        p_year in NUMBER ) RETURN DATE;
+                     
+    FUNCTION get_job_title ( p_employee_id IN NUMBER )  RETURN VARCHAR2;
+    
+    FUNCTION get_dep_name ( p_employee_id IN NUMBER )  RETURN VARCHAR2;
+    
+    FUNCTION get_sum_price_sales(p_table VARCHAR2 DEFAULT 'products') RETURN NUMBER;
+    
+    FUNCTION table_from_list(   p_list_val IN VARCHAR2,
+                                p_separator IN VARCHAR2 DEFAULT ',') RETURN tab_value_list PIPELINED;
+        
+    FUNCTION get_currency (  p_currency IN VARCHAR2 DEFAULT 'USD',
+                             p_exchangedate IN DATE DEFAULT SYSDATE) RETURN tab_exchange PIPELINED;
+                             
+    FUNCTION get_region_cnt_emp ( p_department_id NUMBER default null) RETURN tab_regions_list PIPELINED;
+   
+   --процедуры
+   
+   PROCEDURE check_work_time;
+   
+   PROCEDURE add_new_jobs(p_job_id        IN VARCHAR2,
+                              p_job_title     IN VARCHAR2,
+                              p_min_salary    IN NUMBER,
+                              p_max_salary    IN NUMBER DEFAULT NULL,
+                              po_err          OUT VARCHAR2);
+                              
+    --
+    PROCEDURE del_jobs ( p_job_id    IN VARCHAR2,
+                         po_result   OUT VARCHAR2);
+                         
+    --
+    PROCEDURE update_balance(p_employee_id IN NUMBER,
+                             p_balance IN NUMBER);
+                             
+    -- механізму додавання нового співробітника                         
+    PROCEDURE add_employee (  p_first_name IN VARCHAR2,
+                              p_last_name IN VARCHAR2,
+                              p_email IN VARCHAR2,
+                              p_phone_number IN VARCHAR2,
+                              p_hire_date IN DATE DEFAULT sysdate,
+                              p_job_id IN VARCHAR2,
+                              p_salary IN NUMBER,
+                              p_commission_pct IN VARCHAR2,
+                              p_manager_id IN NUMBER,
+                              p_department_id IN NUMBER );
+   
+   
+END util;
+---------------------------------------------------------------------------------------------
 create or replace PACKAGE BODY util AS
 
 --переменные
