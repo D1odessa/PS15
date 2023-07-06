3113b9b4bc54a73138c8424559a9ae87ec6c8b27
Dima <59514856+D1odessa@users.noreply.github.com>
2023-07-06 12:22:36 +0300

    Create employees_history.sql

---
+++ Tables/employees_history.sql
@@ -0,0 +1,9 @@
+CREATE TABLE employees_history  (   employee_id NUMBER,
+                                    first_name VARCHAR2(25), 
+                                    last_name VARCHAR2(25), 
+                                    email VARCHAR2(35), 
+                                    phone_number VARCHAR2(15),
+                                    start_date DATE,
+                                    end_date DATE ,
+                                    job_id VARCHAR2(10),
+                                    department_id NUMBER );
