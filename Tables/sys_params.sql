16ef1d341f048a801107f99c755d423fd2997d36
Dima <59514856+D1odessa@users.noreply.github.com>
2023-07-11 02:11:29 +0300

    Update sys_params.sql

--- Tables/sys_params.sql
+++ Tables/sys_params.sql
@@ -3,3 +3,12 @@ CREATE TABLE sys_params (   param_name    VARCHAR2(150),
                             value_text         VARCHAR2(2000),
                             value_number NUMBER,
                             param_descr   VARCHAR2(200) );
+
+
+BEGIN
+        INSERT INTO DIMA.sys_params (value_text, param_descr) 
+                VALUES ('USD,EUR,KZT,AMD,GBP,ILS','Список валют для синхронізації в процедурі util.api_nbu_sync');
+                COMMIT;
+
+END;
+/
